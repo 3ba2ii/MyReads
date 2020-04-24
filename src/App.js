bb1cb6b7d5bcd48dbf505f55125fb493554fa1be
Ahmed Ghonem <aghonem2011@gmail.com>
2020-04-24 05:37:34 +0200

    Ready To Submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import * as BooksAPI from "./BooksAPI";
+
 import BookShelfs from "./BookShelfs";
 import SearchPage from "./SearchPage";
 import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
+	state = {
+		apiBooksWithShelfs: [],
+	};
 	render() {
 		return (
 			<div className="app">
--- src/DisplaySearchResult.js
+++ src/DisplaySearchResult.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import * as BooksAPI from "./BooksAPI";
 
+import notFound from "./assets/Not Found.jpg";
 import "./App.css";
 
 class DisplaySearchResult extends Component {
@@ -9,53 +10,46 @@ class DisplaySearchResult extends Component {
 		const shelf = event.target.value;
 		BooksAPI.update(id, shelf);
 	};
-	/*getShelf = (id) => {
-		const selectedShelf = this.state.shelfPerBook.filter(
-			(book) => book.id === id && book.shelf
-		);
+	apiRender() {
+		return this.props.books.map((book) => (
+			<li key={book.id}>
+				<div className="book">
+					<div className="book-top">
+						<div
+							className="book-cover"
+							style={{
+								width: 128,
+								height: 193,
 
-		return selectedShelf;
-	};*/
+								backgroundImage: `url(${book.imageLinks.smallThumbnail})`,
+							}}
+						/>
+
+						<div className="book-shelf-changer">
+							<select id={book.id} onChange={this.onChange} defaultValue="none">
+								<option value="move" disabled>
+									Move to...
+								</option>
+								<option value="currentlyReading">Currently Reading</option>
+								<option value="wantToRead">Want to Read</option>
+								<option value="read">Read</option>
+								<option value="none">None</option>
+							</select>
+						</div>
+					</div>
+					<div className="book-title">{book.title}</div>
+					<div className="book-authors">{book.authors}</div>
+				</div>
+			</li>
+		));
+	}
 
 	render() {
+		console.log(this.props.books);
 		if (this.props.books.length) {
-			return this.props.books.map((book) => (
-				<li key={book.id}>
-					<div className="book">
-						<div className="book-top">
-							<div
-								className="book-cover"
-								style={{
-									width: 128,
-									height: 193,
-
-									backgroundImage: `url(${book.imageLinks.smallThumbnail})`,
-								}}
-							/>
-
-							<div className="book-shelf-changer">
-								<select
-									id={book.id}
-									onChange={this.onChange}
-									defaultValue="none"
-								>
-									<option value="move" disabled>
-										Move to...
-									</option>
-									<option value="currentlyReading">Currently Reading</option>
-									<option value="wantToRead">Want to Read</option>
-									<option value="read">Read</option>
-									<option value="none">None</option>
-								</select>
-							</div>
-						</div>
-						<div className="book-title">{book.title}</div>
-						<div className="book-authors">{book.authors}</div>
-					</div>
-				</li>
-			));
+			return this.apiRender();
 		} else {
-			return <div className="list-books-title">No Books Found</div>;
+			return <img src={notFound} alt="Not Found :(" height="60%" width="65%" />;
 		}
 	}
 }
