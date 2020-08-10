9549e1e4fbbdfa049d0dbc2ea29bff81f397021d
Ahmed Ghonem <aghonem2011@gmail.com>
2020-08-10 20:13:32 +0200

    Updates

---
+++ precache-manifest.8d1d20ae9a3d12c3d79976c413454a07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "72663f6e9a8dafd62eef",
+    "url": "/MyReads/static/css/main.7ff2a3f2.chunk.css"
+  },
+  {
+    "revision": "72663f6e9a8dafd62eef",
+    "url": "/MyReads/static/js/main.72663f6e.chunk.js"
+  },
+  {
+    "revision": "00f619561eeb098fd078",
+    "url": "/MyReads/static/js/1.00f61956.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "ace9d988eac68e2666906666fa4f06fb",
+    "url": "/MyReads/static/media/Blurred Image Many Old Books On Bookshelf In Library _ Stock Photo - Image of antique, archive_ 81866606.ace9d988.jpeg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "ae74bf036e5ca28c08c2846bf17178d2",
+    "url": "/MyReads/static/media/No results - empty states.ae74bf03.png"
+  },
+  {
+    "revision": "5a8ad1d7b310402d2b7533bbd65ca690",
+    "url": "/MyReads/index.html"
+  }
+];
